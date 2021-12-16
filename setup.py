cd00ac0656b1a52fb3202f336b01cd2004adee95
nasrimohtadi <mohtadi.nasri@focus-corporation.com>
2021-12-16 14:35:59 +0100

    added .pypirc / Jenkinsfile / Procfile / setup.py renamed my_app_ to sources Added tests dir

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='DjangoProject',
+    version='0.3dev',
+    description='packaging for my first Django app',
+    packages=find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='mohtadi.nasri@focus-corporation.com',
+    url='https://github.com/focuscorp/Django-project',
+    platforms='Ubuntu'
+)
